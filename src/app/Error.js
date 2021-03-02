718d9154981d6562af69a8b081bca4eeaa0f9108
Sebastian Glaser <hakt0r@users.noreply.github.com>
2021-03-02 12:37:13 +0100

    error icon :D

--- src/app/Error.js
+++ src/app/Error.js
@@ -3,13 +3,29 @@ import { makeStyles }            from '@material-ui/core';
 import { MdRemoveCircleOutline } from 'react-icons/md';
 
 const useStyles = makeStyles( theme => ({
+  center: {
+    textAlign: 'center'
+  },
   loader: {
     width: '90vw',
-    maxHeight: '80vh'
+    height: '90vh',
+    fill: theme.palette.primary.main,
+    maxHeight: 'calc( 90vh - 56px )', 
+      maxWidth: 'calc( 90vh - 56px )', 
+    [`${theme.breakpoints.up('xs')} and (orientation: landscape)`]: { 
+      maxHeight: 'calc( 90vh - 48px )', 
+      maxWidth: 'calc( 90vh - 48px )', 
+    }, 
+    [theme.breakpoints.up('sm')]: { 
+      maxHeight: 'calc( 90vh - 64px )', 
+      maxWidth: 'calc( 90vh - 64px )', 
+    }
   }
 }));
 
 export default function Loading() {
   const classes = useStyles();
-  return <MdRemoveCircleOutline className={classes.loader}/>;
+  return <div className={classes.center}>
+    <MdRemoveCircleOutline color="primary" className={classes.loader}/>
+  </div>;
 }
